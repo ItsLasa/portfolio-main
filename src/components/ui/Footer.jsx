d1746ac22cc3cfd18b44bbeb3fc43c11b33ca009
ItsLasa <manojlasantha306@gmail.com>
2024-11-28 19:03:59 +0530

    add some text

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -4,20 +4,20 @@ export default function Footer() {
   }
 
   return (
-    <footer className="mt-14 flex items-end justify-between px-5 py-4 sm:flex text-body-4 md:text-body-3">
+    <footer className="flex items-end justify-between px-5 py-4 mt-14 sm:flex text-body-4 md:text-body-3">
       <div className="flex flex-col md:flex-row md:w-[62.5vw] lg:w-[57.5vw] justify-between">
         <div className="flex space-x-1" >
           <span>&copy;</span>
           <span>2023</span>
-          <span className="font-extrabold uppercase 2xl:text-body-1">Huy Nguyen</span>
+          <span className="font-extrabold uppercase 2xl:text-body-1">Manoj Lasantha</span>
         </div>
         <div>
-          <span className=" text-body-4 2xl:text-body-1">Site designed and coded with ❤️‍🔥</span>
+          <span className=" text-body-4 2xl:text-body-1">Site designed and coded with NextJs/FramerMotion❤️‍🔥</span>
         </div>
       </div>
-      <button onClick={toTop} className="col-span-2 flex items-center space-x-2 w-fit group 2xl:text-body-1">
-        <span className="font-extrabold uppercase hover:font-black duration-200">BACK TO TOP</span>
-        <span className="group-hover:-translate-y-3 duration-300 ease-in-out">
+      <button onClick={toTop} className="flex items-center col-span-2 space-x-2 w-fit group 2xl:text-body-1">
+        <span className="font-extrabold uppercase duration-200 hover:font-black">BACK TO TOP</span>
+        <span className="duration-300 ease-in-out group-hover:-translate-y-3">
           <svg
             className="-rotate-90"
             width="24"
