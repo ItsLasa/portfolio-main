91d79f5ce40b35b2166bad98f111f291b3a1f357
ItsLasa <manojlasantha306@gmail.com>
2024-12-03 21:43:18 +0530

    add skills and items

--- src/components/homepage/Services.jsx
+++ src/components/homepage/Services.jsx
@@ -1,33 +1,48 @@
 import ServiceUi from "../ui/ServiceUi";
 import Heading from "../ui/Heading";
+import IconCloudDemo from "../IconCloudDemo";
 
 export default function Services() {
   const expertiseItems = [
     "Web Development",
-    "Web Design",
+    "App Development",
     "Wireframing",
     "UI/UX Design",
-    "Branding",
+    
   ];
 
   const toolBoxItems = [
-    "JavaScript",
-    "HTML",
-    "CSS",
-    "Figma",
-    "Webflow",
-    "GSAP",
+    "NextJs",
     "TailwindCSS",
-    "ReactJS",
+    "React-Native",
+    "Figma",
+    "SpringBoot",
+    "AWS",
+    "Mysql",
+    
   ];
+  // const skills=[
+  //   'React',
+  //   'Nextjs',
+  //  'JavaScript',
+  //  ' Typescript',
+  //   'Node',
+  //  ' Python',
+  //  ' Go',
+  //   'Postgres',
+  //   'Docker',
+  //   'Kubernetes',
+  //   'Java',
+  //  ' C++',
+  // ]
 
   return (
     <section id="services" className="my-[10%]" aria-label="services">
       <Heading title="services" />
-      <div className="space-y-14">
+      <div className="space-y-14 ">
         <ServiceUi
           title="my expertises."
-          description="I focus on all things design and web related. With each of my
+          description="I focus on all things design and web/mobile app related. With each of my
           services, my goal is to deliver an impactful and elevating
           digital experience for everyone."
           items={expertiseItems}
@@ -37,6 +52,15 @@ export default function Services() {
           description="These are my go to tech stack to make any projects happen. I am always eager of learning more about my current stack, and new technologies that could expand my horizons."
           items={toolBoxItems}
         />
+        <div className="flex items-center justify-center ">
+        <IconCloudDemo/>
+        </div>
+     
+            {/* <ServiceUi
+          title="my skills."
+          description="These are my go to tech stack to make any projects happen. I am always eager of learning more about my current stack, and new technologies that could expand my horizons."
+          items={skills}
+        /> */}
       </div>
     </section>
   );
