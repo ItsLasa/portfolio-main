0f4fe0c6b2ff93a8b202dd2f61be64ac84f486bd
ItsLasa <manojlasantha306@gmail.com>
2024-12-03 21:42:58 +0530

    heading styles changes

--- src/components/ui/Heading.jsx
+++ src/components/ui/Heading.jsx
@@ -6,7 +6,7 @@ export default function Heading({ title }) {
       <div className="select-none section-heading ">
         <div className="heading flex translate-y-56 items-center justify-center space-x-[3%]">
           {/* <HeadingSVG /> */}
-          <h2 className="text-6xl font-medium uppercase font-DotGothic16 w-fit sm:text-heading-2 text-zinc-600">
+          <h2 className="mb-8 text-5xl font-medium lowercase text-sky-950 w-fit sm:text-heading-2">
             {title}
           </h2>
           {/* <HeadingSVG /> */}
