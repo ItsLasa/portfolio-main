48efcdf6230f97f0e5ca43a2c75c832e10889bad
ItsLasa <manojlasantha306@gmail.com>
2024-12-04 11:49:20 +0530

    change text content

--- src/components/homepage/Role.jsx
+++ src/components/homepage/Role.jsx
@@ -1,7 +1,7 @@
 
 export default function Role({forwardedRef}) {
   return (
-    <section ref={forwardedRef} id="about" className="select-none  flex my-20 md:my-[12%]  py-24 flex-col items-center justify-center overflow-hidden nav-change" aria-label="">
+    <section ref={forwardedRef} id="about" className="select-none  flex my-20 md:my-[12%]  py-20 flex-col items-center justify-center overflow-hidden nav-change" aria-label="">
       <div className="flex items-center w-full space-x-20 ">
         <h1 className="text-heading-1 font-medium text-secondary-400 leading-[1.25em] md:leading-[1.08em]">
         I create elevating digital experiences that inspire and connect with people through development and design.
