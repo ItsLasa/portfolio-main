378803e7fe11f2d8ae9c0303cfb1385a0779ebb4
ItsLasa <manojlasantha306@gmail.com>
2025-04-27 19:07:17 +0530

    fix scroll dark view

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -14,7 +14,8 @@ export default function Contact() {
   useEffect(() => {
     ScrollTrigger.create({
       trigger: contactSection.current,
-      start:"200px bottom",
+      start:"100px bottom",
+
 
       // markers: true,
       animation: gsap
